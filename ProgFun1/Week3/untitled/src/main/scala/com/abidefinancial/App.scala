f0077693b8558c3c6208b450a081322edd99b072
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2016-08-04 23:25:12 +0100

    all

---
+++ ProgFun1/Week3/untitled/src/main/scala/com/abidefinancial/App.scala
@@ -0,0 +1,9 @@
+package com.abidefinancial
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ ProgFun1/example/example/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Matt on 11/06/2016.
+  */
+import example.Lists._
+
+object Main extends App {
+  println(max(List(1,3,2)))
+}
